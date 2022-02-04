0f650bc2bbc36ee818998395eac6ecdb18f73f42
Miguel Lima <miguellimaoliver97@gmail.com>
2022-02-04 18:31:38 -0300

    Estrutura dos cards

---
+++ main.js
@@ -0,0 +1,21 @@
+const cardBoard = document.querySelector("#cardboard");
+const imgs = [
+  "freddyCard.png",
+  "ghostfaceCard.png",
+  "hellraiserCard.png",
+  "leatherfaceCard.png",
+  "michaelCard.png",
+  "piramideCard.png"
+];
+
+let cardHTML = "";
+
+imgs.forEach(img => {
+  cardHTML += `<div class="memory-card" data-card="${img}">
+    <img class="front-face" src="img/${img}"/>
+    <img class="back-face" src="img/jigsaw.png">
+  </div>`;
+});
+
+cardBoard.innerHTML = cardHTML + cardHTML;
+
